845e94d526add31a6674e7c755c9ebafa525c556
ArturKhakimullin <60807919+ArturKhakimullin@users.noreply.github.com>
2020-05-29 21:15:44 +0300

    Update Registration.java

--- src/main/java/Game/Registration.java
+++ src/main/java/Game/Registration.java
@@ -3,17 +3,22 @@ package Game;
 import java.util.Scanner;
 
 public class Registration {
-    public static boolean checkName(String name) {
+        public static boolean checkName(String name) {
         if (name.equals("")) {
             System.out.println("Пустое имя, попробуйте ещё раз");
+            return false;
+        } else if (name.equalsIgnoreCase("выйти")) {
+                System.out.println("Возращайтесь обратно, до скорых встреч");
+                System.exit(0);
+        } else if (name.matches(".*\\d.*")) {
+            System.out.println("Имя не должно стостоять из цифр, попробуйте ещё раз");
             return false;
         } else if (name.contains(" ")) {
             System.out.println("Имя не должно быть из двух слов ");
             return false;
-        } else {
-            System.out.println("Имя корректно, продолжайте...");
-            return true;
-        }
+        } else
+            System.out.println("Добро пожаловать " + name.substring(0, 1).toUpperCase() + name.substring(1));
+        return true;
     }
 
     public static String regName() {
@@ -21,8 +26,7 @@ public class Registration {
                 "или можете написать\"выход\" для выхода из игры");
         return new Scanner(System.in).nextLine();
     }
-    public static String firstUpperCase(String word){
-        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    public static String firstUpperCase(String name){
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
-
 }
